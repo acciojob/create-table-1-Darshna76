2540aad9bfc0f5be11bfb9ad4d36b4a4a6555a2c
Darshna76 <117654044+Darshna76@users.noreply.github.com>
2023-04-14 16:41:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ function insert_Row() {
 	var row=document.createElement("tr");
 	var r1=document.createElement("td");
 	var r2=document.createElement("td");
-	r1.innerText="Row2 cell1";
-	r2.innerText="Row2 cell2"
+	r1.innerText="Row2 Cell1";
+	r2.innerText="Row2 Cell2"
 	row.appendChild(r1);
 	row.appendChild(r2);
 	tabel.appendChild(row);
