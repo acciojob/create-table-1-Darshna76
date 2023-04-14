4b26864fc6427a60e146ee8ca882a4881099b192
Darshna76 <117654044+Darshna76@users.noreply.github.com>
2023-04-14 17:12:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function insert_Row() {
 	var row=tabel.insertRow(0);
 	var c1=row.insertCell(0);
 	var c2=row.insertCell(1);
-	c1.innerHTML = "New Cell1 ";
+	c1.innerHTML =  "New Cell1";
   c2.innerHTML = "New Cell2";
 	
 	
